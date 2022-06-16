255d9ea95943c764339c7b9f72291a9448fae42c
Kovalenko <dmitrij.kovalenko.02@privatbank.ua>
2022-06-16 04:33:30 +0300

    remove redundant import

--- src/main/java/com/candidate/task/controller/UserController.java
+++ src/main/java/com/candidate/task/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.candidate.task.controller;
 
 import com.candidate.task.component.UserComponent;
-import com.candidate.task.domain.User;
 import com.candidate.task.model.UserRequest;
 import com.candidate.task.model.UserResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
